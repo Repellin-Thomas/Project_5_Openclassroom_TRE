22f2d2e4828b137963a45214dac9f7f178d93885
Thomas Repellin <repellin.thomas8@gmail.com>
2022-10-18 22:46:53 +0200

    tre cart wip

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,61 +1,97 @@
 //Etape 1 récupèrer le local storage et le classer dans une liste de produit 
 
 class CartProduct {
-    constructor(color, price, quantity, id) {
-        this.id = id;
-        this.price = price;
-        this.color = color;
-        this.quantity = quantity;
+  constructor(color, price, quantity, id, imageUrl, altTxt, name) {
+    this.id = id;
+    this.price = price;
+    this.color = color;
+    this.quantity = quantity;
+    this.imageUrl = imageUrl;
+    this.altTxt = altTxt;
+    this.name = name;
 
-    }
+  }
 }
-let currentStorage = localStorage.getItem("item")
-let cart = []
 
-if (currentStorage) {
-    cart = JSON.parse(currentStorage);
-    cart = cart.map(x => new CartProduct(x.color, x.price, x.quantity, x.id))
-    console.log(cart);
+
+
+let currentStorage = localStorage.getItem("item");
+let cart = [];
+
+if (currentStorage != undefined) {
+  cart = JSON.parse(currentStorage);
+  cart = cart.map(x => new CartProduct(x.color, x.price, x.quantity, x.id, x.imageUrl, x.altTxt, x.name));
 }
 
 
 function createCartHtml(products) {
-    for (let product of products) {
-        //structure globale
-        let elt = document.getElementById('cart__items');
-        let articleElement = document.createElement("article");
-        elt.appendChild(articleElement)
-        articleElement.classList.add("cart__item")
-        articleElement.setAttribute("data-id", product.id)
-        articleElement.setAttribute("data-color", product.color)
-        //image
-        let imageContainer = document.createElement("div")
-        articleElement.appendChild(imageContainer)
-        imageContainer.classList.add(cart__item__img)
-        let image = document.createElement("img")
-        imageContainer.appendChild(image)
-        image.setAttribute("src", "")
-        // nom, prix, couleur choisie
-        let itemContent = document.createElement("div")
-        articleElement.appendChild(itemContent)
-        itemContent.classList.add("cart__item__content")
-        let contentDescription = document.createElement("div")
-        itemContent.appendChild(contentDescription)
-        contentDescription.classList.add("cart__item__content__description")
-        let itemName = document.createElement("h2")
-        let itemColor = document.createElement("p")
-        let itemPrice = document.createElement("p")
+  for (let product of products) {
+    //structure globale
+    let elt = document.getElementById('cart__items');
+    let articleElement = document.createElement("article");
+    elt.appendChild(articleElement);
+    articleElement.classList.add("cart__item");
+    articleElement.setAttribute("data-id", product.id);
+    articleElement.setAttribute("data-color", product.color);
+    //image
+    let imageContainer = document.createElement("div");
+    articleElement.appendChild(imageContainer);
+    imageContainer.classList.add("cart__item__img");
+    let image = document.createElement("img");
+    imageContainer.appendChild(image);
+    image.setAttribute("src", product.imageUrl);
+    image.setAttribute("alt", product.altTxt)
+    // nom, prix, couleur choisie
+    let itemContent = document.createElement("div");
+    articleElement.appendChild(itemContent);
+    itemContent.classList.add("cart__item__content");
+    let contentDescription = document.createElement("div");
+    itemContent.appendChild(contentDescription);
+    contentDescription.classList.add("cart__item__content__description");
+    let itemName = document.createElement("h2");
+    contentDescription.appendChild(itemName)
+    itemName.innerText = product.name;
+    let itemColor = document.createElement("p");
+    contentDescription.appendChild(itemColor)
+    itemColor.innerText = product.color;
+    let itemPrice = document.createElement("p");
+    contentDescription.appendChild(itemPrice)
+    itemPrice.innerText = product.price + "€";
 
-        // quantité et gestion de la quantité 
-        let contentSettings = document.createElement("div")
-        itemContent.appendChild(contentSettings)
-        contentSettings.classList.add("cart__item__content__settings")
+    // quantité et gestion de la quantité 
+    let contentSettings = document.createElement("div");
+    itemContent.appendChild(contentSettings);
+    contentSettings.classList.add("cart__item__content__settings");
+    let itemQuantity = document.createElement("p")
+    contentSettings.appendChild(itemQuantity)
+    itemQuantity.innerText = "Qté: " + product.quantity
+    let quantityInput = document.createElement("input")
+    contentSettings.appendChild(quantityInput)
+    quantityInput.classList.add("itemQuantity")
+    quantityInput.setAttribute("type", "number")
+    quantityInput.setAttribute("min", 1)
+    quantityInput.setAttribute("max", 100)
+    quantityInput.setAttribute("value", product.quantity)
+    quantityInput.setAttribute("name", "itemQUantity")
+    //bouton delete 
+    let deleteButton = document.createElement("p")
+    contentSettings.appendChild(deleteButton)
+    deleteButton.classList.add("deleteItem")
+    deleteButton.innerText = "Supprimer"
 
-        //bouton delete 
-
-    }
+  }
 
 }
+
+createCartHtml(cart);
+
+
+
+
+
+
+
+
 
 // Etape 2 afficher les produits dans le HTML grace aux informations du localStorage
 /*  L'architecture du HTML telle qu'elle doit être
--- front/js/product.js
+++ front/js/product.js
@@ -8,11 +8,14 @@ var pageId = url.substring(url.lastIndexOf('=') + 1);
 
 //TODO rajouter Nom  imageUrl Alt txt a la classe car nécéssaire dans 
 class CartProduct {
-    constructor(color, price, quantity, id = pageId) {
+    constructor(id, color, price, quantity, imageUrl, altTxt, name) {
         this.id = id;
-        this.price = price;
         this.color = color;
+        this.price = price;
         this.quantity = quantity;
+        this.altTxt = altTxt;
+        this.imageUrl = imageUrl;
+        this.name = name;
 
     }
 }
@@ -21,10 +24,11 @@ class CartProduct {
 let currentStorage = localStorage.getItem("item");
 let cart = []
 
+
 //Si il y un local storage, on le met dans cart en recréant des object de classe (car ce n'est pas le cas dans le local storage)
-if (currentStorage) {
+if (currentStorage != undefined) {
     cart = JSON.parse(currentStorage);
-    cart = cart.map(x => new CartProduct(x.color, x.price, x.quantity, x.id))
+    cart = cart.map(x => new CartProduct(x.id, x.color, x.price, x.quantity, x.imageUrl, x.altTxt))
     console.log(cart);
 }
 
@@ -45,6 +49,8 @@ function getOneProduct() {
         });
 
 };
+
+
 
 
 
@@ -73,14 +79,16 @@ function createCurrentProductHtml(product) {
 }
 
 // fonction pour ajouter un produit ainsi que sa quantité : on a l'indice du produit pour le comparer aux produits existants de la liste . 
-function addToCart(color, price, quantity) {
-    let productIndex = isMyProductInCart(color, pageId)
+//TODO rajouter page Id dans add to cart et quand elle est appelée 
+function addToCart(id, color, price, quantity, imageUrl, altTxt, name) {
+    let productIndex = isMyProductInCart(id, color)
     if (productIndex >= 0) {
         cart[productIndex].quantity += quantity;
 
     }
     else {
-        const product = new CartProduct(color, price, quantity);
+
+        const product = new CartProduct(id, color, price, quantity, imageUrl, altTxt, name);
         cart.push(product)
     }
 
@@ -88,8 +96,9 @@ function addToCart(color, price, quantity) {
 
 
 //calculs pour savoir si le produit existe EN RECUPERANT SON INDICE DANS LA LISTE (i) == .find
-function isMyProductInCart(color, id) {
+function isMyProductInCart(id, color) {
     let alreadyInCart = false
+    //todo boucle while
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
         if (color === product.color && id === product.id) {
@@ -101,9 +110,17 @@ function isMyProductInCart(color, id) {
 
 
 
-
+var imageUrl = 0;
+var altTxt = 0;
+var productName = 0;
 getOneProduct().then(function (product) {
-    createCurrentProductHtml(product); //TODO  mettre get current product dans la parenthese 
+    imageUrl = product.imageUrl;
+    altTxt = product.altTxt;
+    productName = product.name;
+
+    createCurrentProductHtml(product);
+    console.log(altTxt);
+    console.log(imageUrl);
 })
 
 
@@ -116,12 +133,13 @@ product.addEventListener('click', function () {
     let color = document.getElementById('colors').value;
     let quantity = document.getElementById('quantity').value;
     let price = document.getElementById('price').textContent
+    let image = document.getElementBy
     // parse int permet de transformer les prix et quantité string en integer
     quantity = parseInt(quantity);
     price = parseInt(price);
     // if color empeche le produit d'être ajouté si il n'y a pas de couleur sélectionnée 
-    if (color) {
-        addToCart(color, price, quantity)
+    if (color != undefined && quantity > 0) {
+        addToCart(pageId, color, price, quantity, imageUrl, altTxt, productName)
         const stringifiedCart = JSON.stringify(cart)
         localStorage.setItem("item", stringifiedCart)
         console.log(cart)
